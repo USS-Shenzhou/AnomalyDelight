9c405b4fa2a7466367996cf0e7bb684a2db9145a
mafuyu33 <jzh654354772@gmail.com>
2024-09-21 23:19:03 +0100

    会跑的末影龙蛋食物（龙蛋篝火配方/平底锅配方）龙蛋放入煎锅，过一会会变成物品A四处跳跃，需要左键或者右键互动到才能拾取，再放入锅中煎能够获得无限时间力量三的食物物品B

---
+++ src/main/java/cn/ussshenzhou/anomalydelight/networking/packet/S2C/HotDragonEGGRandomMoveS2CPacket.java
@@ -0,0 +1,53 @@
+package cn.ussshenzhou.anomalydelight.networking.packet.S2C;
+
+import cn.ussshenzhou.anomalydelight.AnomalyDelight;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
+
+public class HotDragonEGGRandomMoveS2CPacket implements CustomPacketPayload {
+    public static Type<HotDragonEGGRandomMoveS2CPacket> TYPE =
+            new Type<HotDragonEGGRandomMoveS2CPacket>(ResourceLocation.fromNamespaceAndPath(AnomalyDelight.MODID,"hot_dragon_egg_random_move"));
+
+    // stream codec
+    public static final StreamCodec<FriendlyByteBuf, HotDragonEGGRandomMoveS2CPacket> STREAM_CODEC =
+            CustomPacketPayload.codec(HotDragonEGGRandomMoveS2CPacket::write, HotDragonEGGRandomMoveS2CPacket::new);
+    public Vec3 finalVelocity;
+    public int id;
+
+    public HotDragonEGGRandomMoveS2CPacket(int id, Vec3 finalVelocity){
+        this.id=id;
+        this.finalVelocity=finalVelocity;
+    }
+
+    public HotDragonEGGRandomMoveS2CPacket(FriendlyByteBuf buf){
+        this.id=buf.readInt();
+        this.finalVelocity=buf.readVec3();
+    }
+
+    public void write(FriendlyByteBuf pBuffer) {
+        pBuffer.writeInt(this.id);
+        pBuffer.writeVec3(this.finalVelocity);
+    }
+    public static void handle(HotDragonEGGRandomMoveS2CPacket data, IPayloadContext context){
+
+        context.enqueueWork(()->{
+            if(data.id!=-1 && Minecraft.getInstance().level!=null) {
+                Entity entity = Minecraft.getInstance().level.getEntity(data.id);
+                if(entity!=null) {
+                    entity.setDeltaMovement(data.finalVelocity);
+                }
+            }
+        });
+    }
+    @Override
+    public Type<? extends CustomPacketPayload> type() {
+        return TYPE;
+    }
+}
+
