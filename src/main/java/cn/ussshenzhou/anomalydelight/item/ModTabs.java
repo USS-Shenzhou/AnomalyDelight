53bfd7f6d63bb9f52e09eb7b9dfd9a86bb115834
USS-Shenzhou <969840795@qq.com>
2024-09-22 12:21:10 +0800

    rebase main onto Mafish

---
+++ src/main/java/cn/ussshenzhou/anomalydelight/item/ModTabs.java
@@ -0,0 +1,24 @@
+package cn.ussshenzhou.anomalydelight.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import static cn.ussshenzhou.anomalydelight.AnomalyDelight.MODID;
+
+/**
+ * @author mafuyu33
+ */
+public class ModTabs {
+    public static final DeferredRegister<CreativeModeTab> AD_CREATIVE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ANOMALY_DELIGHT =
+            AD_CREATIVE_TABS.register("anomaly_delight",
+                    () -> CreativeModeTab.builder()
+                            .title(Component.translatable("itemGroup.anomaly_delight"))
+                            .icon(() -> ModItems.MAFISH.get().getDefaultInstance())
+                            .displayItems((itemDisplayParameters, output) -> output.accept(ModItems.MAFISH.get())).build());
+}
